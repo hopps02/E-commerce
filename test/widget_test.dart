7269c4dfae9d8f42ae1293d9d197bdef4003c551
Ahmed Jihad <moody27358@gmail.com>
2026-04-28 16:37:41 +0300

    FINISH ci/cd

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,9 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group("CI/CD pipeline tests", () {
+    test('pipeline tests works fine', () {
+      expect(true, true);
+    });
+  });
+}
