91fac077d42b09537d1136abe7d514ab194be40e
Ahmed Jihad <moody27358@gmail.com>
2026-05-02 05:19:07 +0300

    chore: setup testing strategy

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  group("CI/CD pipeline tests", () {
-    test('pipeline tests works fine', () {
-      expect(true, true);
-    });
-  });
-}
